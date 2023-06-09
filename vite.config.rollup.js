06875f73dd760a30a61cf1f813c37dd484e6dbb3
JinhyeokLee <jinhyuklee@mymusictaste.com>
2023-06-10 00:44:58 +0900

    fix(vite): Fix yarn dev

--- vite.config.rollup.js
+++ vite.config.rollup.js
@@ -8,24 +8,28 @@ export default {
       objectShorthand: true,
       hoistTransitiveImports: false,
     },
-    manualChunks: {
-      "vnd.react": ["react", "react-dom"],
-      "vnd.ui": [
-        "@mantine/core",
-        "@mantine/hooks",
-        "@mantine/notifications",
-        "@tabler/icons-react",
-      ],
-      "vnd.lodash": [
-        "lodash",
-        "lodash/memoize",
-        "lodash/debounce",
-        "lodash/throttle",
+
+    manualChunks:
+      process.env.NODE_ENV === "production"
+        ? {
+            "vnd.react": ["react", "react-dom"],
+            "vnd.ui": [
+              "@mantine/core",
+              "@mantine/hooks",
+              "@mantine/notifications",
+              "@tabler/icons-react",
+            ],
+            "vnd.lodash": [
+              "lodash",
+              "lodash/memoize",
+              "lodash/debounce",
+              "lodash/throttle",
         "lodash/fp",
       ],
       "vnd.chronoNode": ["chrono-node"],
-      "vnd.gptTokenizer": ["gpt-tokenizer"],
-      "vnd.emojis": ["node-emoji"],
-    },
+            "vnd.gptTokenizer": ["gpt-tokenizer"],
+            "vnd.emojis": ["node-emoji"],
+          }
+        : {},
   },
 };
